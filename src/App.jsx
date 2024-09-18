ed975add3a27462a9c6f02cd5c729521e2e315fb
Yash rana <97224327+YashRana03@users.noreply.github.com>
2024-09-18 17:39:26 +0100

    Improve UI and add tv shows

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import './App.css'
-import Movie from './components/Movie';
+import MoviePoster from './components/MoviePoster';
 
 function App() {
 
-  const [movieData, setMovieData] = useState(null) // stores movie data
-  const [movieGenre, setMovieGenre] = useState(null) // stores the movie genre
-  
-  // Authorization options
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `${import.meta.env.REACT_APP_KEY}` // using environment variable
-
-    }
-  };
+  const [mediaData, setMediaData] = useState(null) // stores movie data
+  const [genreData, setGenreData] = useState() // stores the movie genre
   
   // Obtains the necessary data from the Movie API 
   useEffect(() => {
-    if (!movieData) {
-      fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+
+
+    if (!mediaData) {
+      fetch('https://api.themoviedb.org/3/trending/all/week?language=en-US&api_key=7af7f0da356b6bf29e4f80a35298d70a')
       .then(response => response.json())
-      .then(data => setMovieData(data.results))
+      .then(data => setMediaData(data.results))
       .catch(err => console.error(err));
     }
 
     // obtains the movie genre data
-    if (!movieGenre) {
-      fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+    if (!genreData) {
+      fetch('https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=7af7f0da356b6bf29e4f80a35298d70a')
       .then(response => response.json())
       .then(data =>  {
 
@@ -39,28 +32,54 @@ function App() {
         dict[genre[i].id] = genre[i].name
       
         }
-        setMovieGenre(dict)
+        setGenreData(prevState => ({ ...prevState, genresMovie: dict})) 
       })
       .catch(err => console.error(err));
-    }
 
+      // obtains the shows genre data
+      fetch('https://api.themoviedb.org/3/genre/tv/list?language=en&api_key=7af7f0da356b6bf29e4f80a35298d70a')
+      .then(response => response.json())
+      .then(data =>  {
+
+        // Remodelling the Genre data into look up dictionary for better performance
+        const genre = data.genres
+        let dict = {}
+        for (let i=0; i<genre.length; i++) {
+        dict[genre[i].id] = genre[i].name
+      
+        }
+        setGenreData(prevState => ({ ...prevState, genresShows: dict})) // updating the genre object
+      })
+      .catch(err => console.error(err));
+
+    }
+    
+
+
+    // if (!movieData) {
+    //   fetch('https://api.themoviedb.org/3/discover/movie?include_adult=&include_video=true&language=en-US&page=1&sort_by=popularity.desc&api_key=7af7f0da356b6bf29e4f80a35298d70a')
+    //   .then(response => response.json())
+    //   .then(data => setMovieData(data.results))
+    //   .catch(err => console.error(err));
+    // }
 
   }, [])
 
   // Looping over data and creating Movie elements
-  const movieEl = movieData?.map((movie, i) => {
+  const movieEl = mediaData?.map((media, i) => {
     return ( 
-    <Movie 
+    <MoviePoster 
       key={i} 
-      movie={movie}
-      genres={movieGenre}
+      media={media}
+      genre={genreData}
       />
     )
   })
 
 
-  
-  if (!movieData) return  <h1>Loading</h1>
+
+  // Making all the data necessary is received before returning the JSX for displaying the movie posters
+  if (!mediaData || !genreData?.genresMovie || !genreData?.genresShows ) return  <h1>Loading</h1>
 
   return (
     <>
---
+++ src/components/MoviePoster.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+
+/* eslint-disable react/prop-types */
+export default function MoviePoster({media, genre}) {
+
+    const [isShown, setIshown] = useState(false) // Keeps track of whether the movie description is being displayed
+    
+    const mediaType = media.media_type
+
+    // console.log(genre)
+    // console.log(media)
+    // triggered when user hovers on the movie icon
+    function handleMouseEnter() {
+        setIshown(true)
+    }
+
+    // triggered when the user moves on from movie icon
+    function handleMouseLeave() {
+        setIshown(false)
+    }
+
+    // if (mediaType != "tv" || mediaType != "movie") return null
+
+    // generating the genre tags displayed on the movie poster description (a max of only 3 is displayed)
+    const genreTags = media.genre_ids.map((something, i) => {
+        if (i <= 2) {
+            // if (mediaType == "tv") {
+            //     console.log(genre.genresShows[something])
+            // }
+            
+            return <div className="poster-genre" key={i}>{mediaType == "movie" ? genre.genresMovie[something].slice(0, 100) : genre.genresShows[something]}</div>
+        }
+    })
+    
+
+
+    return (
+        <div className="movie-poster" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <img  src={`http://image.tmdb.org/t/p/original${media.poster_path}`} alt="Movie image" style={{transform: isShown ? "scale(1.3)" : ""}}/>
+            
+            {/* Conditionally rendering the description section */}
+            {isShown && (
+                <div  className="movie-info" >
+                    <h3 className="poster-title">{media?.title || media.name}</h3>
+                    <div className="poster-rating--year">
+                        <div className="year">{media?.release_date ?  media.release_date.slice(0, 4) : `Since ${media.first_air_date.slice(0, 4)}`}</div>
+                        <div className="rating">{`${media?.vote_average}`.slice(0, 3) + " IMDb"}</div>
+                    </div>
+
+                    <p className="poster-description">{media.overview.length  > 85 ? `${media.overview.slice(0, 85)}...` : media.overview}</p>
+                    <div className="poster-genre-list">
+                        {genreTags}
+                    </div>
+                </div>
+             )}
+
+        </div>
+    )
+
+}
